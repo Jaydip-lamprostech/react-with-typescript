7fc0645a895b93400278fcdefad0282c367b3d5d
Jaydip-lamprostech <jaydip.lamprostech@gmail.com>
2022-10-20 14:57:40 +0530

    learning react with typescript

--- src/components/state/User.tsx
+++ src/components/state/User.tsx
@@ -7,26 +7,33 @@ type AuthUser = {
 };
 
 function User() {
-  const [user, setUser] = useState<AuthUser | null>(null);
+  const [user, setUser] = useState<AuthUser>({} as AuthUser);
   const handleLogin = () => {
     setUser({
       name: "Jaydip",
       email: "google@gmail.com",
     });
   };
-  const handleLogout = () => {
-    setUser(null);
-  };
+  // const handleLogout = () => {
+  //   setUser(null);
+  // };
   return (
     <>
       <div>
         <button onClick={handleLogin}>Login</button>
-        <button onClick={handleLogout}>Logout</button>
-        <h3>User name is {user?.name}</h3>
-        <h3>User email is {user?.email}</h3>
+        {/* <button onClick={handleLogout}>Logout</button> */}
+        {/* <h3>User name is {user?.name}</h3> */}
+        {/* <h3>User email is {user?.email}</h3> */}
+        <h3>User name is {user.name}</h3>
+        <h3>User email is {user.email}</h3>
       </div>
     </>
   );
 }
 
 export default User;
+
+// const [user, setUser] = useState<AuthUser | null>(null);
+//  const handleLogout = () => {
+//   setUser(null);
+// };
